c1962a9930d1123d5cba0a91e14b2fbb267f7cdc
aptalca <thedjinn@yahoo.com>
2015-05-22 23:39:20 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Get docker env timezone and set system timezone
+echo "setting the correct local time"
+echo $TZ > /etc/timezone
+export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
+dpkg-reconfigure tzdata
+  
 cp /amazon-echo-bridge-0.1.2.jar /config/
 cd /config
 java -jar -Djava.net.preferIPv4Stack=true amazon-echo-bridge-0.1.2.jar --upnp.config.address=$SERVERIP
