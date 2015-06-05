99a319850a47f90ffcc222827fa2745778c72468
aptalca <thedjinn@yahoo.com>
2015-06-05 16:45:07 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,5 +7,9 @@ export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
 dpkg-reconfigure tzdata
   
 cp /amazon-echo-bridge-0.1.3.jar /config/
+mkdir -p /config/static
+cp /configurator.html /config/static/
 cd /config
+sed -i "s/localhost/$SERVERIP/g" static/configurator.html
+jar -uf amazon-echo-bridge-0.1.3.jar static/configurator.html
 java -jar -Djava.net.preferIPv4Stack=true amazon-echo-bridge-0.1.3.jar --upnp.config.address=$SERVERIP
