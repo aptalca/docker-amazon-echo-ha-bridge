99d666dc014cba6d652079765ed07d491368169c
aptalca <thedjinn@yahoo.com>
2015-08-17 13:11:36 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -6,7 +6,7 @@ echo $TZ > /etc/timezone
 export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
 dpkg-reconfigure tzdata
   
-cp /amazon-echo-bridge-0.2.0.jar /config/
+cp /amazon-echo-bridge-0.2.1.jar /config/
 cd /config
 
 #check to see if SERVERPORT variable is set, if not, set it to default
@@ -14,4 +14,4 @@ if [ -z "$SERVERPORT" ]; then
   SERVERPORT=8080
 fi
 
-java -jar -Djava.net.preferIPv4Stack=true amazon-echo-bridge-0.2.0.jar --upnp.config.address=$SERVERIP --server.port=$SERVERPORT
+java -jar -Djava.net.preferIPv4Stack=true amazon-echo-bridge-0.2.1.jar --upnp.config.address=$SERVERIP --server.port=$SERVERPORT
