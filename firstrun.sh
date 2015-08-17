3c75258b7d638e4595a2e2fec463663b4cc80e87
aptalca <thedjinn@yahoo.com>
2015-08-17 13:32:52 -0400

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -7,14 +7,18 @@ export DEBCONF_NONINTERACTIVE_SEEN=true DEBIAN_FRONTEND=noninteractive
 dpkg-reconfigure tzdata
   
 if [ ! -f /config/amazon-echo-bridge-0.2.1.jar ]; then
-  echo "Updating to new version 0.2.1"
+  echo "Installing version 0.2.1"
   cp /amazon-echo-bridge-0.2.1.jar /config/
+else
+  echo "Using existing version/data"
 fi
+echo "Setting correct permissions"
 chown -R nobody:users /config
 cd /config
 
 #check to see if SERVERPORT variable is set, if not, set it to default
 if [ -z "$SERVERPORT" ]; then
+  echo "SERVERPORT environment not passed, using default port 8080"
   SERVERPORT=8080
 fi
 
